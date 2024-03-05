bace386416156b2e6bd7ce8514d58fda038989a3
NicolasZapata <35777697+NicolasZapata@users.noreply.github.com>
2024-03-05 12:15:02 -0500

    🤸‍♂ [Add] Reference code to the function.

--- models/product_product.py
+++ models/product_product.py
@@ -1,5 +1,4 @@
 from odoo import _, api, fields, models
-import random
 
 
 class ProductProduct(models.Model):
@@ -11,37 +10,8 @@ class ProductProduct(models.Model):
         string="Product Reference",
         tracking=True,
     )
-    product_class_code = fields.Char(
-        "Product Class Code",
-        tracking=True,
-        related="product_class_id.code",
-    )
-    default_code = fields.Char(
-        "Internal Reference",
-        index=True,
-        # default=lambda self: 45,
-        default=45,
-        store=True,
-    )
-    sequency = fields.Char("Sequency", default=lambda self: random.randint(00, 99))
-    categ_code = fields.Char(
-        "Category Code",
-        related="categ_id.code",
-    )
-    product_class_id = fields.Many2one(
-        "product.class",
-        string="Product Class",
-    )
-    brand_code = fields.Char(string="Brand", related="product_brand_id.code")
-    material_code = fields.Char(string="Material", related="product_material_id.code")
-    barcode = fields.Char(
-        "Barcode",
-        # copy=False,
-        compute="_auto_complete_barcode",
-        # index="btree_not_null",
-        help="International Article Number used for product identification.",
-        store=True,
-    )
+    product_reference_code = fields.Char('Product Reference Code', related='product_reference_id.code')
+ 
 
     # Conformación del código de barras
     @api.depends(
@@ -49,7 +19,7 @@ class ProductProduct(models.Model):
         "product_class_code",
         "product_brand_id",
         "product_material_id",
-        "default_code",
+        "product_reference_code",
         "sequency",
         "barcode",
     )
@@ -60,10 +30,10 @@ class ProductProduct(models.Model):
              99      +  99      +     99      +    999   +             99            +     99
 
         Ejemplo:
-        45+02+02+123+01+01
-        45+02+00+000+01+02
-        45+02+01+045+00+03
-        45+02+01+045+12+04
+            45+02+02+123+01+01
+            45+02+00+000+01+02
+            45+02+01+045+00+03
+            45+02+01+045+12+04
         """
         for record in self:
             qr_code = []
@@ -80,16 +50,16 @@ class ProductProduct(models.Model):
             if record.product_brand_id:
                 qr_code.append(record.product_brand_id.code)
             elif not record.product_brand_id.code:
-                qr_code.append("00")
+                qr_code.append("000")
 
             if record.product_material_id:
                 qr_code.append(record.product_material_id.code)
             elif not record.product_material_id:
                 qr_code.append("00")
 
-            if record.default_code:
-                qr_code.append(record.default_code)
-            elif not record.default_code:
+            if record.product_reference_code:
+                qr_code.append(record.product_reference_code)
+            elif not record.product_reference_code:
                 qr_code.append("00")
 
             if record.sequency:
